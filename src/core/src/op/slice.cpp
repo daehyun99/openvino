24bbf3673f23ae8c64cc796812aee7a9158a00a6
김대현 (Kim DaeHyun) <daehyun99@kakao.com>
2026-02-17 07:05:20 +0900

    test

--- src/core/src/op/slice.cpp
+++ src/core/src/op/slice.cpp
@@ -4,159 +4,80 @@
 
 #include "openvino/op/slice.hpp"
 
-#include "bound_evaluate.hpp"
-#include "itt.hpp"
-#include "openvino/core/type/element_iterator.hpp"
-#include "openvino/core/validation_util.hpp"
-#include "openvino/reference/slice.hpp"
-#include "slice_shape_inference.hpp"
+#include "common_op_table.hpp"
+#include "common_translators.hpp"
+#include "helper_ops/complex_type_mark.hpp"
+#include "openvino/op/add.hpp"
+#include "openvino/op/broadcast.hpp"
+#include "openvino/op/convert_like.hpp"
+#include "openvino/op/less.hpp"
+#include "openvino/op/select.hpp"
+#include "openvino/op/shape_of.hpp"
+#include "utils.hpp"
+#include "openvino/op/constant.hpp"
+
+using namespace std;
+using namespace ov::op;
 
 namespace ov {
+namespace frontend {
+namespace tensorflow {
 namespace op {
-namespace {
-std::vector<int64_t> default_axes(const size_t n) {
-    std::vector<int64_t> axes;
-    axes.reserve(n);
-    std::generate_n(std::back_inserter(axes), n, SeqGen<int64_t>(0));
-    return axes;
-}
 
-bool slice_bound_check(const ov::Node* const node) {
-    return ov::have_node_inputs_bounds_set(node, 1, node->get_input_size() - 1);
-}
+OutputVector translate_slice_op(const NodeContext& node) {
+    default_op_checks(node, 3, {"Slice", "SLICE"}, true);
 
-bool slice_no_axes(const Node* const node) {
-    return node->get_input_size() < 5;
-}
-}  // namespace
+    auto input = node.get_input(0);
+    auto start = node.get_input(1);
+    auto size  = node.get_input(2);
 
-namespace v8 {
-using ov::op::v0::Constant;
+    auto complex_mark = as_type_ptr<ComplexTypeMark>(input.get_node_shared_ptr());
 
-Slice::Slice(const Output<Node>& data, const Output<Node>& start, const Output<Node>& stop, const Output<Node>& step)
-    : Op({data, start, stop, step}) {
-    constructor_validate_and_infer_types();
-}
-
-Slice::Slice(const Output<Node>& data,
-             const Output<Node>& start,
-             const Output<Node>& stop,
-             const Output<Node>& step,
-             const Output<Node>& axes)
-    : Op({data, start, stop, step, axes}) {
-    constructor_validate_and_infer_types();
-}
-
-std::shared_ptr<Constant> Slice::get_default_const_axes(const Output<Node>& start) const {
-    const auto& start_pshape = start.get_partial_shape();
-    // Static case
-    if (start_pshape.is_static() && start_pshape.size() == 1) {
-        const auto axes = default_axes(static_cast<size_t>(start_pshape[0].get_length()));
-        return Constant::create(element::i64, start_pshape.get_shape(), axes);
-    } else {
-        // Dynamic case
-        return {};
-    }
-}  // namespace ov
-
-void Slice::validate_and_infer_types() {
-    OV_OP_SCOPE(v8_Slice_validate_and_infer_types);
-
-    if (slice_no_axes(this)) {
-        if (auto axes_const = get_default_const_axes(input_value(1))) {
-            set_argument(4, axes_const);
-        }
+    // 1) complex면 underlying FP 텐서로 "data"를 교체
+    Output<Node> data = input;
+    element::Type complex_part_type;
+    if (complex_mark) {
+        complex_part_type = complex_mark->get_complex_part_type();
+        // issue 예제 스타일과 맞추려면 input_value(0) 사용 권장 :contentReference[oaicite:5]{index=5}
+        data = complex_mark->input_value(0);  // 또는 get_data()가 동일 동작이면 그걸 써도 OK
     }
 
-    const auto input_shapes = ov::util::get_node_input_partial_shapes(*this);
-    const auto output_shapes = shape_infer(this, input_shapes);
+    auto const_one  = create_same_type_const_scalar<int32_t>(start, 1);
+    auto const_zero = create_same_type_const_scalar<int32_t>(start, 0);
 
-    set_input_is_relevant_to_shape(0);
-    for (size_t i = 1; i < get_input_size(); ++i) {
-        NODE_VALIDATION_CHECK(this,
-                              get_input_element_type(i).is_integral_number(),
-                              "Slice `",
-                              slice::shape_names[i - 1],
-                              "` input type must be integer.");
-        set_input_is_relevant_to_shape(i);
+    auto stop_pos = std::make_shared<v1::Add>(start, size);
+
+    // 2) stop_neg는 ShapeOf(data)로 계산 (절대 ShapeOf(input) 쓰지 않기!)
+    Output<Node> stop_neg = std::make_shared<v3::ShapeOf>(data);
+
+    // len(start) = ShapeOf(start) = [N] (1D 텐서)
+    auto start_shape = std::make_shared<v3::ShapeOf>(start);
+
+    // start/stop/step은 1D + 동일 타입이어야 함 :contentReference[oaicite:6]{index=6}
+    auto ind_et = start_shape->get_output_element_type(0); // 보통 i64
+    auto idx0 = v0::Constant::create(ind_et, Shape{1}, {0});
+    auto idx1 = v0::Constant::create(ind_et, Shape{1}, {1});
+
+    // stop_neg = ShapeOf(data)[0 : len(start)]  -> complex 표현에서 마지막 축(2) 제거 목적
+    stop_neg = std::make_shared<v8::Slice>(stop_neg, idx0, start_shape, idx1);
+    stop_neg = std::make_shared<v1::ConvertLike>(stop_neg, size);
+
+    auto negative_sizes_mask = std::make_shared<v1::Less>(size, const_zero);
+    auto stop = std::make_shared<v1::Select>(negative_sizes_mask, stop_neg, stop_pos);
+
+    auto step = std::make_shared<v3::Broadcast>(const_one, start_shape);
+
+    auto slice = std::make_shared<v8::Slice>(data, start, stop, step);
+    set_node_name(node.get_name(), slice);
+
+    if (complex_mark) {
+        auto complex_slice = std::make_shared<ComplexTypeMark>(slice->output(0), complex_part_type);
+        return complex_slice->outputs();
     }
 
-    set_output_type(0, get_input_element_type(0), output_shapes.front());
+    return slice->outputs();
 }
-
-std::shared_ptr<Node> Slice::clone_with_new_inputs(const OutputVector& new_args) const {
-    OV_OP_SCOPE(v8_Slice_clone_with_new_inputs);
-    check_new_args_count(this, new_args);
-    if (new_args.size() == 4) {
-        return std::make_shared<Slice>(new_args.at(0), new_args.at(1), new_args.at(2), new_args.at(3));
-    } else {
-        return std::make_shared<Slice>(new_args.at(0), new_args.at(1), new_args.at(2), new_args.at(3), new_args.at(4));
-    }
-}
-
-bool Slice::has_evaluate() const {
-    OV_OP_SCOPE(v8_Slice_has_evaluate);
-
-    const auto valid_integral_type = [](const element::Type& et) -> bool {
-        switch (et) {
-        case element::i8:
-        case element::i16:
-        case element::i32:
-        case element::i64:
-        case element::u8:
-        case element::u16:
-        case element::u32:
-        case element::u64:
-            return true;
-        default:
-            return false;
-        }
-    };
-
-    return element::is_byte_type(get_input_element_type(0)) && valid_integral_type(get_input_element_type(1)) &&
-           (slice_no_axes(this) || valid_integral_type(get_input_element_type(4)));
-}
-
-bool Slice::can_constant_fold(const OutputVector& inputs) const {
-    return element::is_byte_type(inputs.at(0).get_element_type()) && Op::can_constant_fold(inputs);
-}
-
-bool Slice::evaluate(TensorVector& outputs, const TensorVector& inputs) const {
-    OV_OP_SCOPE(v8_Slice_evaluate);
-
-    if (!element::is_byte_type(inputs[0].get_element_type()))
-        return false;
-
-    const auto output_shapes =
-        shape_infer(this, ov::util::get_tensors_partial_shapes(inputs), make_tensor_accessor(inputs));
-    outputs[0].set_shape(output_shapes.front().to_shape());
-
-    const auto starts = ov::get_tensor_data_as<int64_t>(inputs[1]);
-    const auto steps = ov::get_tensor_data_as<int64_t>(inputs[3]);
-    const auto& axes = slice_no_axes(this) ? default_axes(starts.size()) : ov::get_tensor_data_as<int64_t>(inputs[4]);
-
-    reference::slice(static_cast<const char*>(inputs[0].data()),
-                     inputs[0].get_shape(),
-                     static_cast<char*>(outputs[0].data()),
-                     outputs[0].get_shape(),
-                     inputs[0].get_element_type().size(),
-                     starts,
-                     steps,
-                     axes);
-    return true;
-}
-
-bool Slice::evaluate_lower(ov::TensorVector& output_values) const {
-    return slice_bound_check(this) && default_lower_bound_evaluator(this, output_values);
-}
-
-bool Slice::evaluate_upper(ov::TensorVector& output_values) const {
-    return slice_bound_check(this) && default_upper_bound_evaluator(this, output_values);
-}
-
-bool Slice::evaluate_symbol(TensorSymbolVector& output_symbols) const {
-    return slice_bound_check(this) && ov::util::default_symbol_evaluator(this, output_symbols);
-}
-}  // namespace v8
 }  // namespace op
+}  // namespace tensorflow
+}  // namespace frontend
 }  // namespace ov
